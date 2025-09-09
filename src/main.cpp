adf87f251c835420354e7c12d7ff80675a04d0c4
Zachary Westerman <westerman.zachary@gmail.com>
2025-09-08 20:49:55 -0500

    pull in word search code from old project

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,37 @@
-int main() {
+#include "wordSearch.hpp"
+#include <iostream>
+#include <random>
+#include <z/core/array.hpp>
+#include <z/core/string.hpp>
+#include <z/util/dictionary.hpp>
+
+int main(int argc, char **argv) {
+	if (argc < 3) {
+		std::cerr << "Usage: " << argv[0] << " <rows> <cols>" << std::endl;
+		return 1;
+	}
+
+	const auto rows = zstring(argv[1]).integer();
+	const auto cols = zstring(argv[2]).integer();
+
+	std::cerr << "Loading dictionary... ";
+	z::util::dictionary dict;
+	if (dict.read("/usr/share/dict/words")) {
+		std::cerr << "ERROR: Failed to read dictionary file!" << std::endl;
+		return 1;
+	} else {
+		dict = dict.filter([](const zstring &word) { return word.length() >= 3; });
+		std::cout << zstring::numberFormat(dict.length()) << " words.\n";
+	}
+
+	// Create an initial random grid
+	wordSearch ws;
+	ws.randomize(rows, cols);
+
+	std::cout << "Permuting grid to exclude dictionary words..." << std::endl;
+
+	// Print the grid
+	ws.print(std::cout);
+
 	return 0;
 }
---
+++ src/wordSearch.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <iostream>
+#include <z/core/array.hpp>
+#include <z/core/string.hpp>
+#include <z/util/dictionary.hpp>
+
+struct wordMatch {
+	int x;
+	int y;
+	int width;
+	int height;
+	int direction;
+	z::core::string<> text;
+};
+
+class wordSearch {
+private:
+	uint32_t *data;
+	int *highlighted;
+	int dataWidth;
+	int dataHeight;
+
+	struct matchType {
+		int pos;
+		int dir;
+		int len;
+	};
+
+	z::core::array<matchType> matches;
+	int selectedMatch;
+	int oldColor;
+
+	void setHighlight(int pos, int dir, int len, int color);
+
+public:
+	wordSearch();
+	~wordSearch();
+
+	bool load(std::istream &input);
+	// bool load(const zstring &input);
+	void randomize(int rows, int cols);
+
+	bool loaded() const;
+
+	void clear();
+	void reset();
+
+	int width() const;
+	int height() const;
+
+	int matchCount() const;
+
+	bool select(int index, int color);
+	z::core::string<> getMatch(int index) const;
+
+	wordMatch getMatchData(int index) const;
+
+	void print(std::ostream &output) const;
+
+	int find(const z::core::string<> &text, int color = 8);
+
+	int scan(const z::util::dictionary &dict, int minLength, int maxLength = 0, int color = 8);
+};
